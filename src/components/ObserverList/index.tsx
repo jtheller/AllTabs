c8eda239a8245d376a5934ffa1bbcd222d8ceb92
jtHeller <jateruy@live.cn>
2021-06-22 06:31:53 -0700

    Optimize observer list.

--- src/components/ObserverList/index.tsx
+++ src/components/ObserverList/index.tsx
@@ -2,5 +2,5 @@ import { observer, Observer } from "mobx-react";
 import React from "react";
 
 export const ObserverList = observer(({ list, render, getItemKey }) => (
-  list && list.map((item, i) => <Observer key={getItemKey(item)}>{() => render(item, i)}</Observer>)
+  list && list.map((item, i) => <Observer key={getItemKey(item) || i}>{() => render(item, i)}</Observer>)
 ));
